a50393c388c9b121e7fa294b45ffd07baf4be08f
huangliemin <liemin.huang@gmail.com>
2015-03-11 14:43:48 +0800

    add session and redirect

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -20,12 +20,11 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET','POST'])
 def index():
-	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ''
-	return render_template('index.html',current_time=datetime.utcnow(),name=name,form=form)
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html',current_time=datetime.utcnow(),name=session.get('name'),form=form)
 
 @app.route('/user/<name>')
 def user(name):
