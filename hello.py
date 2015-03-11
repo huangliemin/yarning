ada5dbe2d2e96b9cb38cb86f29cf484f08d1d6c5
huangliemin <liemin.huang@gmail.com>
2015-03-11 15:09:55 +0800

    add flash alert

--- hello.py
+++ hello.py
@@ -23,7 +23,7 @@ def index():
 	form = NameForm()
 	if form.validate_on_submit():
 		old_name = session.get('name')
-		if old_name is not None and old_name != form.new.data:
+		if old_name is not None and old_name != form.name.data:
 			flash('Looks like you have changed your name!')
 		session['name'] = form.name.data
 		return redirect(url_for('index'))
