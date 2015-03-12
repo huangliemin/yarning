a2f2dbac5f2cb497cefd58976ae862bb3a6fcb51
huangliemin <liemin.huang@gmail.com>
2015-03-12 11:03:01 +0800

    add mail

--- hello.py
+++ hello.py
@@ -12,6 +12,8 @@ import os
 from flask.ext.script import Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.mail import Mail, Message
+from threading import Thread
+
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -95,7 +97,13 @@ def send_mail(to, subject, template, **kwargs):
 	msg = Message(app.config['YARNING_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['YARNING_MAIL_SENDER'],recipients=[to])
 	msg.body = render_template(template + '.txt', **kwargs)
 	msg.html = render_template(template + '.html', **kwargs)
-	mail.send(msg)
+	thr = Thread(target=send_async_email, args=[app,msg])
+	thr.start()
+	return thr
+
+def send_async_email(app,msg):
+	with app.app_context():
+		mail.send(msg)
 
 if __name__=="__main__":
 	manager.run()
