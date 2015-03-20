4f1cdcd99accf31f3ce01ee5aab6d15425dbdda1
huangliemin <liemin.huang@gmail.com>
2015-03-20 14:05:46 +0800

    add test

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ class ProductionConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 class TestingConfig(Config):
-	DEBUG = True
+	TESTING = True
 	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 config = {
